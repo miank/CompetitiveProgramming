bff5a88e97f6556afc505d9b5cca3cb73b42501b
Ankit Mishra <ankit1703@gmail.com>
2023-07-27 09:52:54 +0530

    Folder deleted and new solutions added.

---
+++ CodeStudio/DeleteAGivenNode/DeleteAGivenNode.cpp
@@ -0,0 +1,9 @@
+// DeleteAGivenNode.cpp 
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ CodeStudio/ReverseFirstKElementsOfQueue/ReverseFirstKElementsOfQueue.cpp
@@ -0,0 +1,13 @@
+// ReverseFirstKElementsOfQueue.cpp 
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
