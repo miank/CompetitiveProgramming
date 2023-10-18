674072ce02d4295a4d9148d632157366d9e4c747
Ankit Mishra <ankit1703@gmail.com>
2023-10-18 16:59:34 +0530

    Anagram
    Sort the strings and then compare it.

---
+++ GeeksForGeeks/Anagram/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        // Your code here
+        bool res=true;
+        if(a.length()==b.length()){
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(a!=b){
+            res=false;
+        }
+        }
+        else {
+            res=false;
+        }
+        return res;
+    }
+
+};
+
+int main(){
+    string str1, str2;
+    Solution obj;
+    cin >> str1 >> str2;
+    cout << obj.isAnagram(str1, str2) << endl;
+
+    return 0;
+}
