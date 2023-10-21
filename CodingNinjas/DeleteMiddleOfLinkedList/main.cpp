8c07c2c4f9f00aa0d4fbd252593bbaf7b2e541c6
Ankit Mishra <ankit1703@gmail.com>
2023-10-21 12:49:02 +0530

    Middle Of LinkedList

---
+++ CodingNinjas/DeleteMiddleOfLinkedList/main.cpp
@@ -0,0 +1,58 @@
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = nullptr;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = nullptr;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+
+Node* deleteMiddle(Node* head){
+    // Write your code here.
+    Node* slow = head;
+
+    Node* fast = head;
+
+    Node* prev = nullptr;
+
+ 
+
+    while(fast != nullptr && fast->next != nullptr){
+
+        prev = slow;
+
+        slow = slow->next;
+
+        fast = fast->next->next;
+
+    }
+
+    if(prev != nullptr)
+
+    prev->next = slow->next;
+
+    else head = head->next;
+
+    delete slow;
+
+    return head;
+
+
+
+    
+}
