76a457bc3fd756fb68f8558ca7e8ea5a892a72af
Ankit Mishra <ankit1703@gmail.com>
2023-11-04 22:12:16 +0530

    All testcases passed.

---
+++ GeeksForGeeks/MaximuDistanceBetweenSameElements/main.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // your task is to complete this function
+    int maxDistance(int arr[], int n)
+    {
+    //Code here
+
+    unordered_map<int, int> mp;
+
+    int max_dis = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        if(mp.find(arr[i]) == mp.end()){
+            mp[arr[i]] = i;
+        }else{
+            max_dis = max(max_dis, i - mp[arr[i]]);
+        }
+    }    
+    return max_dis;
+
+    }
+};
+
+int main(){
+
+    Solution obj;
+    int *arr = new int[8]{1, 1, 2, 2, 2, 1, 3, 4};
+
+    cout << obj.maxDistance(arr, 8) << endl;
+
+    return 0;
+}
