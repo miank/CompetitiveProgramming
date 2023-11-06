e8006810815d62f46050db5b268d02f43c84b12c
Ankit Mishra <ankit1703@gmail.com>
2023-11-06 14:03:15 +0530

    All testcases passed.

--- CodingNinjas/RotatedArray/RotatedArray.cpp
+++ CodingNinjas/RotatedArray/RotatedArray.cpp
@@ -1,20 +1,48 @@
-// RotatedArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// RotatedArray.cpp 
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rotateArray(vector<int>arr, int k) {
+    // Write your code here.
+
+
+    int i = 1;
+    int n = arr.size();
+
+    while(i <= k)
+    {
+            int temp = arr[0];
+            for (int i = 0; i < n - 1; i++)
+            {
+                arr[i] = arr[i + 1];                
+            }
+
+            arr[n - 1] = temp;
+            i++;
+    }
+
+    return arr;        
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> v{1,2,3,4,5};
+    int k;
+
+    vector<int> v1;
+    cin >> k;
+
+    v1 = rotateArray(v, k);
+
+    for (int i = 0; i < v1.size(); i++)
+    {
+        /* code */
+        cout << v1[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
