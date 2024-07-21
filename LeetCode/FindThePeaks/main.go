d5aaf7c88b4a53b33ffa186182376c5efa8141de
Ankit Mishra <ankit1703@gmail.com>
2024-07-21 16:14:22 +0530

    Code drafted.

---
+++ LeetCode/FindThePeaks/main.go
@@ -0,0 +1,11 @@
+package main
+
+func findPeaks(mountain []int) []int {
+	var peaks []int
+	for i := 1; i < len(mountain)-1; i++ {
+		if mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1] {
+			peaks = append(peaks, i)
+		}
+	}
+	return peaks
+}
